ebd32bc1e2f43895c40d64f3c690fb0fd591ec01
Manuel Lang <manuellang183@gmail.com>
2021-09-26 08:39:31 +0200

    fix: paths

--- api/main.py
+++ api/main.py
@@ -66,7 +66,8 @@ def create_video(request_data: APIInput) -> Dict:
     output_path_video = speech_to_video.generate_video(model_path="/app/model_checkpoints/wav2lip.pth", video_path="/app/input_video/output_video.mp4", audio_path=output_audio_path)
     logger.info(f'The generated audio file is in {output_path_video} .')
 
-    output_path_video = f'../ui/public/{output_path_video.split("/")[-1]}'
+    new_output_path_video = f'../ui/public/{output_path_video.split("/")[-1]}'
+    copyfile(output_path_video, new_output_path_video)
 
     # return FileResponse(output_path_video)
-    return {'url': output_path_video}
+    return {'url': new_output_path_video.split("/")[-1]}
